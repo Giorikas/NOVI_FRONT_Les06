fadbc371c268415d787dab8d1c930fc4f446dc00
Giorikas <jjphendrikx@hotmail.com>
2023-03-27 21:00:57 +0200

    opdrachten klaar

---
+++ loops-and-arrays.js
@@ -0,0 +1,67 @@
+// ==========================================
+// Opdracht 1
+// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
+// ==========================================
+
+const names = ["Henk", "Piet", "Fred", "Joop"];
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
+// Na jouw script zie je de aangepaste waardes:
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + "je");
+}
+
+
+
+// ==========================================
+// Opdracht 2
+// Schrijf een script dat ieder EVEN getal in onderstaande array met 2 vermenigvuldigd, en ieder ONEVEN getal met 3
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const numbers = [2, 4, 5, 29, 38];
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [2, 4, 5, 29, 38];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]*2);
+}
+
+
+
+// ==========================================
+// Opdracht 3
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array, het volume uitrekent.
+// Het volume van een vierkant is Lengte x Breedte x Hoogte
+// Vervolgens moet de huidige waarde in de array overschreven worden met: "Het volume van [a] is [b]"
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const squares = [30, 2, 8, 24, 11];
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(squares) geeft: [30, 2, 8, 24, 11];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(squares) geeft:
+// [
+//   'Het volume van 30 is 27000',
+//   'Het volume van 2 is 8',
+//   'Het volume van 8 is 512',
+//   'Het volume van 24 is 13824',
+//   'Het volume van 11 is 1331'
+// ]
+
+for (let i = 0; i < squares.length; i++) {
+    console.log(`Het volume van ${squares[i]} is ${squares[i]**3}`)
+}
+
---
+++ loops.js
@@ -0,0 +1,101 @@
+// ==========================================
+// Opdracht 1. Maak een for loop die het volgende patroon in de terminal print:
+// Verwachte uitkomsten:
+// *
+// **
+// ***
+// ****
+// *****
+
+// Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
+// Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+// ==========================================
+console.log("Opdracht 1");
+for (let i = 1; i < 6; i++) {
+    let str = "*";
+    console.log(`${str.repeat(i)}`);
+}
+console.log("");
+// ==========================================
+// Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
+// Verwachte uitkomsten:
+// loop...
+// loop...
+// loop...
+// loop...
+// klaar!
+// ==========================================
+console.log("Opdracht 2");
+for (let i = 1; i < 5; i++) {
+    let str = "loop...";
+    console.log(str);
+}
+console.log("klaar!");
+console.log("");
+
+
+// ==========================================
+// Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
+// Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
+// Verwachte uitkomsten:
+// 0
+// 1
+// 2
+// > 3
+// > 4
+// > 5
+// >> 6
+// >> 7
+// >> 8
+// >> 9
+// ==========================================
+console.log("Opdracht 3");
+for (let i = 0; i < 10; i++) {
+    switch (i) {
+        case 0: case 1: case 2:
+            console.log(i);
+            break;
+        case 3: case 4: case 5:
+            console.log("\> " + i);
+            break;
+        default:
+            console.log("\>\> " + i);
+    }
+}
+console.log("");
+
+
+// ==========================================
+// Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
+// Echter, voor veelvouden van 3 print je "Fizz" in plaats van het nummer, en voor veelvouden van 5 print je "Buzz".
+// Voor getallen die zowel een veelvoud van 3 als van 5 zijn, print je "FizzBuzz"
+// Verwachte uitkomsten:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+
+for (let i = 0; i < 100; i++) {
+
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else console.log(i);
+}
+
+
+
